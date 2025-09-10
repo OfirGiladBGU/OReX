3324272b0d3aedbc73e6eafe73b7872c6f113dc2
OfirGiladBGU <ofirgila@post.bgu.ac.il>
2025-09-11 01:45:23 +0300

    small fix

--- run_Slicer.py
+++ run_Slicer.py
@@ -9,5 +9,5 @@ for filename in os.listdir(input_folder):
     if filename.endswith('.obj'):
         input_path = os.path.join(input_folder, filename)
         cmd = ['python3', 'Slicer.py', input_path, output_folder, num]
-        print(f"Running: {' '.join(cmd)}")
-        subprocess.run(cmd)
+        print(f"Running in background: {' '.join(cmd)}")
+        subprocess.Popen(cmd)
