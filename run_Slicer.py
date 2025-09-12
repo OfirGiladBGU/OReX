5af1f525ca14e18abdd4a95b3f2f972565a906f3
OfirGiladBGU <ofirgila@post.bgu.ac.il>
2025-09-13 00:11:06 +0300

    updated script

--- run_Slicer.py
+++ run_Slicer.py
@@ -3,11 +3,19 @@ import subprocess
 
 input_folder = './parse/input'
 output_folder = './parse/slices'
+log_folder = './log'
 num = '100'
 
-for filename in os.listdir(input_folder):
+# Create log folder if it doesn't exist
+os.makedirs(log_folder, exist_ok=True)
+
+filenames = sorted(os.listdir(input_folder))
+for idx, filename in enumerate(filenames):
+    if idx + 1 < 46:
+        continue
     if filename.endswith('.obj'):
         input_path = os.path.join(input_folder, filename)
-        cmd = ['python3', 'Slicer.py', input_path, output_folder, num]
-        print(f"Running in background: {' '.join(cmd)}")
-        subprocess.Popen(cmd)
+        log_file = os.path.join(log_folder, f"{filename}.log")
+        cmd = f"nohup python3 Slicer.py '{input_path}' '{output_folder}' '{num}' > '{log_file}' 2>&1 &"
+        print(f"Running in background: {cmd}")
+        subprocess.Popen(cmd, shell=True)
