d48ee067f9c3fb42fab6dc40832cd8086269e32d
OfirGiladBGU <ofirgila@post.bgu.ac.il>
2025-09-13 16:44:39 +0300

    added run main + update run slicer

--- run_Main.py
+++ run_Main.py
@@ -2,21 +2,19 @@ import os
 import subprocess
 import pathlib
 
-input_folder = './parse/input'
-output_folder = './parse/slices'
-log_folder = './log1'
-num = '100'
+input_folder = './parse/slices'
+output_folder = './parse/output'
+log_folder = './log2'
+num = '0'
 
 # Create log folder if it doesn't exist
 os.makedirs(log_folder, exist_ok=True)
 
-filenames = sorted(pathlib.Path(input_folder).glob("*.obj"))
+filenames = sorted(pathlib.Path(input_folder).glob("*.csl"))
 for idx, filename in enumerate(filenames):
     filename = filename.name
-    if idx + 1 < 46:
-        continue
     input_path = os.path.join(input_folder, filename)
     log_file = os.path.join(log_folder, f"{filename}.log")
-    cmd = f"nohup python3 Slicer.py '{input_path}' '{output_folder}' '{num}' > '{log_file}' 2>&1 &"
+    cmd = f"nohup python3 Main.py '{output_folder}' '{input_path}' --cuda_device '{num}' > '{log_file}' 2>&1 &"
     print(f"Running in background: {cmd}")
     subprocess.Popen(cmd, shell=True)
